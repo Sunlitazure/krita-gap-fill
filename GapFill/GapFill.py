1d366bc3f87d7be250338278290308c4d0210c01
SunlitAzure <technochicken@gmail.com>
2023-02-20 23:16:16 -0700

    fix issue with embedded groups by putting gapFill layer at top of group

--- GapFill/GapFill.py
+++ GapFill/GapFill.py
@@ -39,7 +39,7 @@ class GapFill(Extension):
 		if type(lineArtLayer) == GroupLayer:
 			parent = lineArtLayer
 			if len(parent.childNodes()) != 0:
-				bottomLayer = parent.childNodes()[0]
+				bottomLayer = parent.childNodes()[-1]
 		else:
 			bottomLayer = lineArtLayer
 			parent = bottomLayer.parentNode()
@@ -48,7 +48,7 @@ class GapFill(Extension):
 
 		self.app.action("selectopaque").trigger() 
 		self.currentDoc.waitForDone ( )
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 				
 		for layer in self.currentDoc.topLevelNodes():
 			if type(layer) == SelectionMask:
@@ -58,32 +58,32 @@ class GapFill(Extension):
 				break
 
 		selection.grow(9, 9)
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		selection.shrink(9, 9, False)
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		self.currentDoc.setActiveNode(gapFillLayer)
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		
 		eraseAction = self.app.action("erase_action")
 		if eraseAction.isChecked():
 			eraseAction.trigger()
 			self.currentDoc.waitForDone ( )
-			self.currentDoc.refreshProjection()
+			#self.currentDoc.refreshProjection()
 
 		self.app.action('fill_selection_foreground_color').trigger()
 		self.currentDoc.waitForDone ( )
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		self.currentDoc.setActiveNode(lineArtLayer)
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		self.app.action("selectopaque").trigger() 
 
 		self.currentDoc.waitForDone ( )
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		for layer in self.currentDoc.topLevelNodes():
 			if type(layer) == SelectionMask:
@@ -93,39 +93,41 @@ class GapFill(Extension):
 				break
 				
 		self.currentDoc.setActiveNode(gapFillLayer)
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		#Krita.instance().action("clear").trigger()
 		#self.currentDoc.setActiveNode(gapFillLayer)
 		#self.currentDoc.refreshProjection()
 		
 		selection.cut(gapFillLayer)
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		self.app.action("selectopaque").trigger()
 		
 		self.currentDoc.waitForDone ( )
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		selection.border(1,1)
 		selection.invert()
 		selection.cut(gapFillLayer)
 		self.app.action("deselect").trigger()
 		self.currentDoc.waitForDone ( )
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
+		
 		
 		self.currentDoc.rootNode().removeChildNode(gapFillLayer)
 		parent.addChildNode(gapFillLayer, bottomLayer)
-		parent.removeChildNode(bottomLayer)
-		parent.addChildNode(bottomLayer, gapFillLayer)
-		self.currentDoc.refreshProjection()
+		if type(lineArtLayer) != GroupLayer:
+			parent.removeChildNode(bottomLayer)
+			parent.addChildNode(bottomLayer, gapFillLayer)
+			#self.currentDoc.refreshProjection()
 		
 		gapFillLayer.setOpacity(3)
-		self.currentDoc.refreshProjection()
+		#self.currentDoc.refreshProjection()
 		
 		lineArtLayer.setColorLabel(8) #gray label
-		for layer in lineArtLayer.childNodes():
-			layer.setColorLabel(8)
+		#for layer in lineArtLayer.childNodes():
+		#	layer.setColorLabel(8)
 		self.currentDoc.refreshProjection()
 		
 		
